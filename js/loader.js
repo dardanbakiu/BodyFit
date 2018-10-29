b10f2061d9d517b60d392fbd6a9aea02603ea016
Dardan Bakiu <dardan.bakiu@gmail.com>
2018-10-29 20:58:00 +0100

    js
    css

---
+++ js/loader.js
@@ -0,0 +1,9 @@
+
+
+var overlay = document.getElementById("overlay");
+
+window.addEventListener('load', function(){
+  overlay.style.display = 'none';
+})
+
+//Ktu loaderi esht mundsu me funksionu veq kur t'ka delay pernime
