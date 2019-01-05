fced6d5f8c98c22fa268ed5d0e9801434e0ef602
Blerina Fejza <Blerinafejza98@gmail.com>
2019-01-05 23:02:03 +0100

    validimi update &loader delete

--- loader.js
+++
@@ -1,9 +0,0 @@
-
-
-var overlay = document.getElementById("overlay");
-
-window.addEventListener('load', function(){
-  overlay.style.display = 'none';
-})
-
-//Ktu loaderi esht mundsu me funksionu veq kur t'ka delay pernime
